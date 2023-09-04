5165bf868f262d672b132d101fcbe45cd02990a7
Gabriel De almeida torres <gabrieldealmeidatorres@gmail.com>
2023-09-04 12:59:51 +0100

    finishing sed

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,17 +1,38 @@
 #include "File.hpp"
 
-int main(void) { 
-	std::fstream filename;
 
-	filename.open("file.txt", std::ios::out); // Creates a file with first parameter as the name
-	if (!filename) { 
+int main(int ac, char **av) { 
+	std::ifstream	source;
+	std::ofstream	destiny;
+	std::string		checker;
+	std::string		output;
+
+	if (ac != 4){ // Check if there are valid number of arguments as the exercise asks
+		std::cout << "Wrong number of arguments!" << std::endl;
+		return 0;
+	}
+	source.open(av[1]); // Open the first parameter if its a valid file, else will return an error
+	if (!source.is_open()) { 
+		std::cout << "Error on opening file" << std::endl;
+		return 0;
+	}
+	output = av[1];
+	output += ".replace";
+	destiny.open(output.c_str(), std::ios::out); // Try to create the output file so I can write the new text
+	if (!destiny.is_open()) {
 		std::cout << "Error on creating file" << std::endl;
+		return 0;
 	}
-	else {
-		
-		while (1)
-		std::cout << "File created!" << std::endl;
-		filename.close();
+	while (!source.eof())
+	{
+		while (getline(source, checker))
+		{
+			if (checker == av[2])
+			{
+				
+			}
+		}
 	}
-	std::cout << "Testing" << std::endl;
+	source.close();
+	destiny.close();
 }
