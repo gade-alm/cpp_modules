be4437d8851db90739127427b9a6751b55ef1695
gade-alm <gabrieldealmeidatorres@gmail.com>
2023-09-27 12:09:28 +0100

    character done

--- cpp04/ex03/Character.cpp
+++ cpp04/ex03/Character.cpp
@@ -28,3 +28,38 @@ Character::~Character() {
 			delete _inventory[i];
 }
 
+std::string	const& Character::getName()  const{
+	return _name;
+}
+
+void	Character::equip(AMateria* m) {
+	for (int i = 0; i < 4; i++)
+	{
+		if (_inventory[i] == NULL) {
+			_inventory[i] = m;
+			std::cout << "Materia equipped successfully!" << std::endl;
+			return ;
+		}
+	}
+	std::cout << "Full inventory, can't equip new materia" << std::endl;
+}
+
+void	Character::unequip( int idx ) {
+	if (idx > -1 && idx < 4) {
+		if (_inventory[idx] != NULL)
+			_inventory[idx] = NULL;
+		else
+		std::cout << "No materia equipped on the slot" << std::endl;
+	}
+	else
+		std::cout << "Invalid slot number, please try again" << std::endl;
+}
+
+void	Character::use( int idx, ICharacter& target ) { 
+	if (idx > -1 && idx < 4) {
+		if (_inventory[idx] != NULL)
+			_inventory[idx]->use(target);
+	}
+	else
+		std::cout << "Invalid slot number, please try again" << std::endl;
+}
--- cpp04/ex03/MateriaSource.cpp
+++ cpp04/ex03/MateriaSource.cpp
@@ -1,12 +1,19 @@
 #include "MateriaSource.hpp"
 
 MateriaSource::MateriaSource() {
+	for (int i = 0; i < 4; i++)
+		_materiasInventory[i] = NULL;
 }
 
 MateriaSource::~MateriaSource() {
+	for (int i = 0; i < 4; i++)
+		if (_materiasInventory[i])
+			delete _materiasInventory[i];
 }
 
 MateriaSource::MateriaSource( const MateriaSource& obj ) { 
+	for (int i = 0; i < 4; i++)
+		_materiasInventory[i] = NULL;
 	*this = obj;
 }
 
@@ -16,9 +23,9 @@ MateriaSource& MateriaSource::operator=( const MateriaSource& obj ) {
 }
 
 void	MateriaSource::learnMateria( AMateria* ) {
-
+	
 }
 
 AMateria* MateriaSource::createMateria( std::string const & type ) {
-	
+
 }
