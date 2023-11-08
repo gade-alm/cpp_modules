b735f1beac0ac8aa1b23a61213efa812e752e95f
gade-alm <gabrieldealmeidatorres@gmail.com>
2023-11-08 10:23:40 +0000

    Fix on pointer

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -7,10 +7,10 @@ int main( void )
 	std::string *ptr = &example;
 	std::string &ref = example;
 
-	std::cout << "Memory Adress string:" << &example << std::endl;
-	std::cout << "Memory Adress pointer:" << &ptr << std::endl;
-	std::cout << "Memory Adress reference:" << &ref << std::endl;
-	std::cout << "Value of string:" << example << std::endl;
-	std::cout << "Value of pointer:" << *ptr << std::endl;
-	std::cout << "Value of reference:" << ref << std::endl;
+	std::cout << "Memory Adress string:    " << &example << std::endl;
+	std::cout << "Memory Adress pointer:   " << ptr << std::endl;
+	std::cout << "Memory Adress reference: " << &ref << std::endl;
+
+	std::cout << "Value of pointer:        " << *ptr << std::endl;
+	std::cout << "Value of reference:      " << ref << std::endl;
 }
