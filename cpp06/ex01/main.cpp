43615f14fd381133b19b363a170cc2bd5d4d694e
gade-alm <gabrieldealmeidatorres@gmail.com>
2024-01-25 08:37:37 +0000

    more tests on cpp06/ex01

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,11 +6,12 @@ int main ( void ) {
 	Data*		ptr;
 	uintptr_t	value;
 
-	std::cout << "Value of original name: " <<original.name << std::endl << "Value of original age: " << original.age << std::endl;
+	std::cout << "Value of original name: " << original.name << std::endl << "Value of original age: " << original.age << std::endl;
 	std::cout << "------SERIALIZING------" << std::endl;
 	value = Serializer::serialize(&original);
 	std::cout << "------DESERIALIZING------" << std::endl;
 	ptr = Serializer::deserialize(value);
-	std::cout << "Value of ptr name: " << ptr->name << std::endl << "Value of ptr age: " <<ptr->age << std::endl;
+	std::cout << &original << "  " << &*ptr << std::endl;
+	std::cout << "Value of ptr name: " << ptr->name << std::endl << "Value of ptr age: " << ptr->age << std::endl;
 	}
 }
