3a27610c9c826a94f5b27be779ac1789e9472f88
gade-alm <gabrieldealmeidatorres@gmail.com>
2024-02-01 10:18:34 +0000

    cleaning some of the code

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -7,43 +7,43 @@
 #include <iterator>
 #include <list>
 
-template<typename T, typename Container = std::deque<T> >
-class MutantStack : public std::stack<T, Container> {
+template< typename T >
+class MutantStack : public std::stack<T> {
 	private:
 	public:
 		MutantStack( void );
 		MutantStack( const MutantStack& copy );
 		MutantStack& operator=( const MutantStack& copy );
 		~MutantStack( void );
-		typedef typename Container::iterator iterator;
+		typedef typename std::deque<T>::iterator iterator;
 		iterator begin( void );
 		iterator end( void );
 };
 
-template<typename T, typename Container>
-MutantStack<T, Container>::MutantStack( void ) {
+template<typename T>
+MutantStack<T>::MutantStack( void ) {
 }
 
-template<typename T, typename Container>
-MutantStack<T, Container>::MutantStack( const MutantStack& copy ) {	
+template<typename T>
+MutantStack<T>::MutantStack( const MutantStack& copy ) {	
 	*this = copy;
 }
 
-template<typename T, typename Container>
-MutantStack<T, Container>& MutantStack<T, Container>::operator=( const MutantStack& copy ){
+template<typename T>
+MutantStack<T>& MutantStack<T>::operator=( const MutantStack& copy ){
 }
 
-template<typename T, typename Container>
-MutantStack<T, Container>::~MutantStack( void ){
+template<typename T>
+MutantStack<T>::~MutantStack( void ){
 }
 
-template<typename T, typename Container>
-typename MutantStack<T, Container>::iterator	MutantStack<T, Container>::begin( void ) {
+template<typename T>
+typename MutantStack<T>::iterator	MutantStack<T>::begin( void ) {
 		return this->c.begin();	
 }
 
-template<typename T, typename Container>
-typename	MutantStack<T,Container>::iterator	MutantStack<T, Container>::end( void ) {
+template<typename T>
+typename	MutantStack<T>::iterator	MutantStack<T>::end( void ) {
 	return this->c.end();
 }
 
