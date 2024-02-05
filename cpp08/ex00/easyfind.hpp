547ed9c15fa752fd31068ddca0a30fd117d5706f
Gabriel De almeida torres <gade-alm@c2r7s6.42lisboa.com>
2024-02-05 11:14:45 +0000

    small fix on easyfind

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -18,8 +18,7 @@ int	easyfind(T parameter, int value) {
 		std::cout << "Value found" << std::endl;
 		return 1;
 	}
-	else
-		std::cout << "Could not find the value on the list" << std::endl;
+	std::cout << "Could not find the value on the list" << std::endl;
 	return 0;
 }
 
